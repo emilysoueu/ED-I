343ea93556739416bbd97c07c1813fd4ce4679bf
Emily Costa <emilys2costa@gmail.com>
2018-12-08 17:50:54 -0300

    Update enunciado.cpp

--- Tree/enunciado.cpp
+++ Tree/enunciado.cpp
@@ -1,21 +1,13 @@
 /**
 Um laboratório de pesquisa deseja manter informações de seus colaboradores de maneira organizada e de fácil recuperação.
-
-A ideia é se poder cadastrar as informações dos colaboradores em múltiplas árvores que possam ser consultadas com múltiplus objetivos.
-
+A ideia é se poder cadastrar as informações dos colaboradores em múltiplas árvores que possam ser consultadas com
+múltiplus objetivos.
 A principio, as árvores terão apenas as informações de nome, matrícula, data de entrada e data de saida de cada colaborador.
-
 E as consultas deverão ser realizadas por nome, data de entrada, data de saída.
-
 As operações que devem ser implementadas:
-
  - cadastro de colaborador
-
  - edição de colaborador
-
  - consulta de colaborador --> por 1 dos critérios
-
- 
 
 * Você deverá ter 1 árvore para cada parâmetro de consulta
 
